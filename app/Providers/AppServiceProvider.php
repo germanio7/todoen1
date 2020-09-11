8cb9cbbf17aacefb433a04f0b0923bfd8ebc1ce7
germanio 7 <german.e.escobar@gmail.com>
2020-09-11 19:09:58 -0300

    ip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         $visits = DB::table('visits')->get();
-        dd($visits);
+        dd(request()->ip());
         if (count($visits) > 0) {
             foreach ($visits as $item) {
                 if ($item->ip != request()->ip() || $item->date != now()->format('d-m-Y')) {

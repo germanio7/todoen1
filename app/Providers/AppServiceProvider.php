3f9f77f120d87810c3b2dce3d9f3d1db06b80fd9
germanio 7 <german.e.escobar@gmail.com>
2020-09-12 18:00:37 -0300

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $aux = DB::table('visits')->get();
+        dd($aux);
+
         $visits = DB::table('visits')->where('ip', request()->ip())->where('date', now()->format('d-m-Y'))->get();
 
         if (count($visits) < 1) {

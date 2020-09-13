0192b0edf56114c8b70ab93b1c37622250b31fa6
germanio 7 <german.e.escobar@gmail.com>
2020-09-13 18:14:33 -0300

    avanzando

--- public/js/app.js
+++ public/js/app.js
@@ -2163,10 +2163,41 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
+  data: function data() {
+    return {
+      games: [{
+        game: "Mortal Kombat 3",
+        url: "https://www.retrogames.cc/embed/29633-mortal-kombat-3-usa.html"
+      }, {
+        game: "Super Mario Bros.",
+        url: "https://www.retrogames.cc/embed/16843-super-mario-bros-japan-usa.html"
+      }]
+    };
+  },
   components: {
     layout: _Shared_Layout__WEBPACK_IMPORTED_MODULE_0__["default"]
+  },
+  methods: {
+    setGame: function setGame(game) {
+      var aux = document.getElementById("juego");
+      aux.setAttribute("src", game);
+    }
   }
 });
 
@@ -39791,12 +39822,42 @@ var render = function() {
     "div",
     [
       _c("layout", [
+        _c(
+          "div",
+          {
+            staticClass:
+              "flex justify-center font-mono text-4xl md:text-6xl text-gray-900"
+          },
+          [_vm._v("\n            Juegos\n        ")]
+        ),
+        _vm._v(" "),
+        _c(
+          "div",
+          { staticClass: "flex-wrap md:flex justify-center px-6 pt-4 pb-2" },
+          _vm._l(_vm.games, function(item) {
+            return _c(
+              "span",
+              {
+                key: item.id,
+                staticClass:
+                  "cursor-pointer inline-block bg-purple-200 rounded-full px-3 py-1 text-sm font-semibold text-purple-700 mr-2 mb-2",
+                on: {
+                  click: function($event) {
+                    return _vm.setGame(item.url)
+                  }
+                }
+              },
+              [_vm._v("#" + _vm._s(item.game))]
+            )
+          }),
+          0
+        ),
+        _vm._v(" "),
         _c("div", { staticClass: "flex justify-center" }, [
-          _vm._v("\n            MK3\n            "),
           _c("iframe", {
             attrs: {
-              src:
-                "https://www.retrogames.cc/embed/29633-mortal-kombat-3-usa.html",
+              id: "juego",
+              src: "",
               width: "800",
               height: "500",
               frameborder: "no",
--- resources/js/Pages/Games.vue
+++ resources/js/Pages/Games.vue
@@ -1,10 +1,24 @@
 <template>
     <div>
         <layout>
-            <div class="flex-wrap justify-center">
-                MK3
+            <div
+                class="flex justify-center font-mono text-4xl md:text-6xl text-gray-900"
+            >
+                Juegos
+            </div>
+            <div class="flex-wrap md:flex justify-center px-6 pt-4 pb-2">
+                <span
+                    v-for="item in games"
+                    :key="item.id"
+                    @click="setGame(item.url)"
+                    class="cursor-pointer inline-block bg-purple-200 rounded-full px-3 py-1 text-sm font-semibold text-purple-700 mr-2 mb-2"
+                    >#{{ item.game }}</span
+                >
+            </div>
+            <div class="flex justify-center">
                 <iframe
-                    src="https://www.retrogames.cc/embed/29633-mortal-kombat-3-usa.html"
+                    id="juego"
+                    src=""
                     width="800"
                     height="500"
                     frameborder="no"
@@ -21,7 +35,29 @@
 <script>
 import layout from "./Shared/Layout";
 export default {
-    components: { layout }
+    data() {
+        return {
+            games: [
+                {
+                    game: "Mortal Kombat 3",
+                    url:
+                        "https://www.retrogames.cc/embed/29633-mortal-kombat-3-usa.html"
+                },
+                {
+                    game: "Super Mario Bros.",
+                    url:
+                        "https://www.retrogames.cc/embed/16843-super-mario-bros-japan-usa.html"
+                }
+            ]
+        };
+    },
+    components: { layout },
+    methods: {
+        setGame(game) {
+            let aux = document.getElementById("juego");
+            aux.setAttribute("src", game);
+        }
+    }
 };
 </script>
 <style></style>

70757bc9cf4f24d91a53dd6eddbfb3876cd3d3fa
germanio 7 <german.e.escobar@gmail.com>
2020-09-13 21:42:14 -0300

    games

--- public/js/app.js
+++ public/js/app.js
@@ -2177,16 +2177,98 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
   data: function data() {
     return {
       games: [{
+        game: "After Burner II",
+        url: "https://www.retrogames.cc/embed/29861-after-burner-ii-usa-europe.html"
+      }, {
+        game: "ATP Tour Championship Tennis",
+        url: "https://www.retrogames.cc/embed/30481-atp-tour-championship-tennis-usa.html"
+      }, {
+        game: "Bare Knuckle (Streets of Rage)",
+        url: "https://www.retrogames.cc/embed/30497-bare-knuckle-ikari-no-tetsuken-streets-of-rage-world.html"
+      }, {
+        game: "Comix Zone",
+        url: "https://www.retrogames.cc/embed/29773-comix-zone-usa.html"
+      }, {
+        game: "Contra",
+        url: "https://www.retrogames.cc/embed/16841-contra-usa.html"
+      }, {
+        game: "Demolition Man",
+        url: "https://www.retrogames.cc/embed/30488-demolition-man-usa-europe.html"
+      }, {
+        game: "Desert Strike",
+        url: "https://www.retrogames.cc/embed/18464-desert-strike-usa-europe.html"
+      }, {
+        game: "Earthworm Jim",
+        url: "https://www.retrogames.cc/embed/30211-earthworm-jim-usa.html"
+      }, {
+        game: "FIFA International Soccer",
+        url: "https://www.retrogames.cc/embed/29938-fifa-international-soccer-usa-europe-en-fr-de-es.html"
+      }, {
+        game: "FIFA Soccer 97",
+        url: "https://www.retrogames.cc/embed/30117-fifa-soccer-97-usa-europe-en-fr-de-es-it-sv.html"
+      }, {
+        game: "FIFA 98 - Road to World Cup",
+        url: "https://www.retrogames.cc/embed/30223-fifa-98-road-to-world-cup-europe-en-fr-es-it-sv.html"
+      }, {
+        game: "International Superstar Soccer Deluxe",
+        url: "https://www.retrogames.cc/embed/30521-international-superstar-soccer-deluxe-europe.html"
+      }, {
+        game: "Mortal Kombat II",
+        url: "https://www.retrogames.cc/embed/30510-mortal-kombat-ii-world.html"
+      }, {
         game: "Mortal Kombat 3",
         url: "https://www.retrogames.cc/embed/29633-mortal-kombat-3-usa.html"
       }, {
+        game: "NBA Hang Time",
+        url: "https://www.retrogames.cc/embed/28432-nba-hang-time-usa.html"
+      }, {
+        game: "NBA Jam Tournament Edition",
+        url: "https://www.retrogames.cc/embed/30412-nba-jam-tournament-edition-world.html"
+      }, {
+        game: "Rock n' Roll Racing",
+        url: "https://www.retrogames.cc/embed/28493-rock-n-roll-racing-usa.html"
+      }, {
         game: "Super Mario Bros.",
         url: "https://www.retrogames.cc/embed/16843-super-mario-bros-japan-usa.html"
+      }, {
+        game: "Super Mario Kart",
+        url: "https://www.retrogames.cc/embed/16887-super-mario-kart-usa.html"
+      }, {
+        game: "Taz-Mania",
+        url: "https://www.retrogames.cc/embed/19017-taz-mania-world.html"
+      }, {
+        game: "Tiny Toon Adventures",
+        url: "https://www.retrogames.cc/embed/28522-tiny-toon-adventures-buster-s-hidden-treasure-usa.html"
+      }, {
+        game: "Ultimate Mortal Kombat 3",
+        url: "https://www.retrogames.cc/embed/16951-ultimate-mortal-kombat-3-usa.html"
+      }, {
+        game: "Vectorman",
+        url: "https://www.retrogames.cc/embed/28386-vectorman-usa-europe.html"
       }]
     };
   },
@@ -39831,29 +39913,68 @@ var render = function() {
           [_vm._v("\n            Juegos\n        ")]
         ),
         _vm._v(" "),
-        _c(
-          "div",
-          { staticClass: "flex-wrap md:flex justify-center px-6 pt-4 pb-2" },
-          _vm._l(_vm.games, function(item) {
-            return _c(
-              "span",
-              {
-                key: item.id,
-                staticClass:
-                  "cursor-pointer inline-block bg-purple-200 rounded-full px-3 py-1 text-sm font-semibold text-purple-700 mr-2 mb-2",
-                on: {
-                  click: function($event) {
-                    return _vm.setGame(item.url)
-                  }
-                }
-              },
-              [_vm._v("#" + _vm._s(item.game))]
-            )
-          }),
-          0
-        ),
-        _vm._v(" "),
         _c("div", { staticClass: "flex justify-center" }, [
+          _c("div", { staticClass: "inline-block relative w-64" }, [
+            _c(
+              "select",
+              {
+                staticClass:
+                  "block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+              },
+              [
+                _c("option", { attrs: { disabled: "", selected: "" } }, [
+                  _vm._v("Seleccionar juego")
+                ]),
+                _vm._v(" "),
+                _vm._l(_vm.games, function(item) {
+                  return _c(
+                    "option",
+                    {
+                      key: item.id,
+                      on: {
+                        click: function($event) {
+                          return _vm.setGame(item.url)
+                        }
+                      }
+                    },
+                    [_vm._v(_vm._s(item.game))]
+                  )
+                })
+              ],
+              2
+            ),
+            _vm._v(" "),
+            _c(
+              "div",
+              {
+                staticClass:
+                  "pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
+              },
+              [
+                _c(
+                  "svg",
+                  {
+                    staticClass: "fill-current h-4 w-4",
+                    attrs: {
+                      xmlns: "http://www.w3.org/2000/svg",
+                      viewBox: "0 0 20 20"
+                    }
+                  },
+                  [
+                    _c("path", {
+                      attrs: {
+                        d:
+                          "M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+                      }
+                    })
+                  ]
+                )
+              ]
+            )
+          ])
+        ]),
+        _vm._v(" "),
+        _c("div", { staticClass: "flex justify-center mt-4" }, [
           _c("iframe", {
             attrs: {
               id: "juego",

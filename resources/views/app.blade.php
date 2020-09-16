03ee2180359a16e723ea733429d07dd95a779886
germanio 7 <german.e.escobar@gmail.com>
2020-09-16 19:17:18 -0300

    fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,11 +3,11 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    <title>Radios + Clima + Noticias + Juegos</title>
+    <title>Todo en uno</title>
     <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
     <link rel="icon" type="image/svg" href="./audio.svg">
     {{-- <link rel="stylesheet" href="{{asset('css/main.css')}}"> --}}
-    <script src="{{ mix('/js/app.js?v='.filemtime(public_path('js/app.js'))) }}" defer></script>
+    <script src="{{ asset('/js/app.js?v='.filemtime(public_path('js/app.js'))) }}" defer></script>
   </head>
   <body class="bg-gray-100">
     @inertia
